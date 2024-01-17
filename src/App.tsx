508c5418661bffced12ed2463dc0134793349c4f
briggsrr <robert.briggs.3@icloud.com>
2024-01-16 18:55:56 -0800

    rb - adding hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import './App.css'
-
-import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import { Amplify } from 'aws-amplify';
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
+import { withAuthenticator, Button, Heading, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import { type AuthUser } from "aws-amplify/auth";
-import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
+import config from './amplifyconfiguration.json';
+Amplify.configure(config);
 
-type AppProps = {
-  signOut?: UseAuthenticator["signOut"]; //() => void;
-  user?: AuthUser;
-};
-
-const App: React.FC<AppProps> = ({ signOut, user }) => {
-  
+export function App({ signOut, user }: WithAuthenticatorProps) {
   return (
-    <div style={styles.container}>
-      <Heading level={1}>Hello {user?.username}</Heading>
+    <>
+    <View>
+      <Heading>Hello {user?.username} </Heading>
       <Button onClick={signOut}>Sign out</Button>
-    </div>
+    </View>
+    </>
+
   );
-};
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 20,
-  },
-
-} as const;
+}
 
 export default withAuthenticator(App);
