9b1bd32e8c5ee04047bf30e6905ebdcb5162a782
briggsrr <robert.briggs.3@icloud.com>
2024-01-16 19:28:34 -0800

    rb - getting image uploading to work

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "skinImages": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
-import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { withAuthenticator, Button, Heading, View } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-import config from './amplifyconfiguration.json';
-Amplify.configure(config);
+import { uploadData } from 'aws-amplify/storage';
 
 export function App({ signOut, user }: WithAuthenticatorProps) {
+  // Function to handle file selection and upload
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files) return;
+    const file = event.target.files[0];
+    const filename = file.name;
+
+    try {
+      const result = await uploadData({
+        key: filename,
+        data: file
+      }).result;
+      console.log('Succeeded: ', result);
+    } catch (error) {
+      console.log('Error : ', error);
+    }
+  };
+
+  const triggerFileInput = () => {
+    document.getElementById('fileInput')?.click();
+  };
+
   return (
     <>
-    <View>
-      <Heading>Hello {user?.username} </Heading>
-      <Button onClick={signOut}>Sign out</Button>
-    </View>
+      <View>
+        <Heading>Hello {user?.username} </Heading>
+        <Button onClick={signOut}>Sign out</Button>
+        <input
+          type="file"
+          id="fileInput"
+          style={{ display: 'none' }}
+          onChange={handleFileChange}
+        />
+        <Button onClick={triggerFileInput}>Upload Image</Button>
+      </View>
     </>
-
   );
 }
 
